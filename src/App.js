c779643c0546bf2a31dd3e22bcd366ad51c57b42
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-01-26 02:02:53 +0600

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.scss';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Home from "./Pages/Home/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import logo from "../../../Images/logo.png";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <section className="header">
+      <Navbar fixed="top" className="header" collapseOnSelect expand="lg">
+        <Container>
+          <Navbar.Brand as={HashLink} to="/home">
+            <img src={logo} alt="Maxon" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav className="menu">
+              <Link to="/home">Home</Link>
+              <HashLink to="/home">About</HashLink>
+              <HashLink to="/home">Features</HashLink>
+              <HashLink to="/home">Project</HashLink>
+              <HashLink to="/home">Blog</HashLink>
+              <HashLink to="/home">Contact</HashLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </section>
+  );
+};
+
+export default Header;
