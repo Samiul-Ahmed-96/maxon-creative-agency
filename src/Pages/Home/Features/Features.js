86d4f87bd53afcfe9457b8b6a1f5299767c4929a
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-01-26 15:08:07 +0600

    Added Features

--- src/Pages/Home/Features/Features.js
+++ src/Pages/Home/Features/Features.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import './Features.scss';
 
 const Features = () => {
-    return (
-        <section className='features-main' id='features'>
-            
-        </section>  
-    );
+  const [features, setFeatures] = useState([]);
+  useEffect(() => {
+    fetch("/features.json")
+      .then((res) => res.json())
+      .then((data) => setFeatures(data));
+  }, []);
+  return (
+    <section className="features-main" id="features">
+        <div className="section-heading">
+            <span>Special Features</span>
+            <h1>Advertising with Difference</h1>
+        </div>
+      <Container className="banner-container">
+        <Row>
+          {features.map((feature) => (
+            <Col key={feature.id} lg={3} md={3} sm={12} xs={12}>
+              <div className="feature-content">
+                <img src={feature.img} alt="" />
+                <h3>{feature.name}</h3>
+                <p>{feature.description}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Features;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
+import Features from '../Features/Features';
 
 const Home = () => {
     return (
         <main>
             <Banner/>
             <About/>
+            <Features/>
         </main>
     );
 };
